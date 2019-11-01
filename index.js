d16a312b18a79f889eac127ef754514bfb0007df
onlyfullstack <onlyfullstackdev@gmail.com>
2019-11-01 23:18:37 +0530

    added event handlers

--- index.js
+++ index.js
@@ -7,16 +7,25 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      counter: 0
+      counter: 0,
+      counterToggle: true,
+      counterToggleText: "Hide Counter"
     };
+
+    this.handleEvent = this.handleEvent.bind();
   }
+
+handleEvent() {
+  this.state = { counterToggle: !this.state.counterToggle}
+}
 
   render() {
     return (
       <div>
         <div>
           <p>Event Handler</p>
-          <button>Click me</button>
+          <button onClick={this.handleEvent}>Simple Event Handler</button>
+          <button onClick={this.handleEvent}>{this.state.counterToggleText}</button>
         </div>
         <div style={{"margin-top": "50px"}}>
           <button>Increment</button>
